ca6f2228b4d5581100abbb4f2f869e4da2ca599c
Fredrik Jansson <fj@avassa.io>
2024-04-29 13:50:04 +0200

    Setup typescript/vue

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -46,10 +46,49 @@ vim.g.rustaceanvim = {
 }
 
 require('mason').setup({})
+
 require('mason-lspconfig').setup({
-  ensure_installed = {'rust_analyzer', 'pylsp'},
+  ensure_installed = {
+    'rust_analyzer',
+    'pylsp',
+    'tsserver',
+    'volar',
+  },
   handlers = {
     lsp_zero.default_setup,
     rust_analyzer = lsp_zero.noop,
+    volar = function()
+      require('lspconfig').volar.setup({})
+    end,
+    tsserver = function()
+      local vue_typescript_plugin = require('mason-registry')
+      .get_package('vue-language-server')
+      :get_install_path()
+      .. '/node_modules/@vue/language-server'
+      .. '/node_modules/@vue/typescript-plugin'
+
+      require('lspconfig').tsserver.setup({
+        init_options = {
+          plugins = {
+            {
+              name = "@vue/typescript-plugin",
+              location = vue_typescript_plugin,
+              languages = {'javascript', 'typescript', 'vue'}
+            },
+          }
+        },
+        filetypes = {
+          'javascript',
+          'javascriptreact',
+          'javascript.jsx',
+          'typescript',
+          'typescriptreact',
+          'typescript.tsx',
+          'vue',
+        },
+      })
+    end,
   },
 })
+
+
