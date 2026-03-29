4e797610562aaafc27accd4751436fdcf3dacf4e
Fredrik Jansson <fredrik.jansson.se@gmail.com>
2026-03-29 18:29:04 +0200

    Removed unused dep

--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -30,8 +30,7 @@ return {
   {
     'nvim-telescope/telescope.nvim',
     branch = 'master',
-    dependencies = { { 'nvim-lua/plenary.nvim' },
-      { 'BurntSushi/ripgrep' } }
+    dependencies = { { 'nvim-lua/plenary.nvim' } }
   },
   {
     { 'neovim/nvim-lspconfig' }
