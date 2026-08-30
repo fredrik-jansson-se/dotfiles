1ed32f2210372e7deb2e2c371586e8478638b9c2
Fredrik Jansson <fredrik.jansson.se@gmail.com>
2026-08-30 19:59:50 +0200

    remove cpp, treesitter fix

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -1,12 +1,9 @@
 require'nvim-treesitter'.install { 'rust', 'dockerfile' }
 
--- require('nvim-treesitter.configs').setup({
---   highlight = { enable = true },
---   indent = { enable = true },
--- })
---
--- -- Treesitter-based folding
--- vim.opt.foldmethod = 'expr'
--- vim.opt.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
--- vim.opt.foldenable = false  -- open all folds by default
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { 'rust', 'dockerfile' },
+  callback = function()
+    vim.treesitter.start()
+  end,
+})
 
